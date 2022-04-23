6c88127c0d0f0d123824aecd696ebb813a9f7694
Berkan Çimen <berkan.cimen@adesso.com.tr>
2022-04-23 14:45:34 +0300

    Initial Commit

---
+++ OKR1 2022 Movie App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OKR1 2022 Movie App
+//
+//  Created by Berkan Cimen on 23.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OKR1 2022 Movie App/OKR1_2022_Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKR1_2022_Movie_AppApp.swift
+//  OKR1 2022 Movie App
+//
+//  Created by Berkan Cimen on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct OKR1_2022_Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
